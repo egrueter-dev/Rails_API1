ce0197eb2b329154e8c92f9fa755caef4457d847
Erik Grueter <erikgrueter@gmail.com>
2015-04-24 16:05:44 -0400

    Adds constraints an rspec testing for ApiConstraints

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
-      # we are going to list our resources here
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
     end
   end
 end
